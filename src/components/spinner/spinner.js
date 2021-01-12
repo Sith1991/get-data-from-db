c2b4c43c33eade6ed90ea04676ff13891632a11b
Fiodor Furick <fff24.06.91@gmail.com>
2021-01-12 21:14:01 +0200

    create take API data from server and create loading indicator

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-ydzi11b0qcj">
+            <div className="ldio-7q1i0warmxh">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
